1d7c9bc5dc5cf19c9afd23563f573739719e0ffc
FwilsonRetro <fwilson@retrorabbit.co.za>
2021-02-03 11:10:31 +0200

    Players model created
    A player model has been created to allow for querysnapshots data to be easily converted and shown onto the application screen

---
+++ lib/models/players.dart
@@ -0,0 +1,16 @@
+class Players {
+  final String name;
+  final String position;
+  final int attack;
+  final int midfield;
+  final int defense;
+  final int goalkeeping;
+
+  Players(
+      {this.name,
+      this.position,
+      this.attack,
+      this.midfield,
+      this.defense,
+      this.goalkeeping});
+}
