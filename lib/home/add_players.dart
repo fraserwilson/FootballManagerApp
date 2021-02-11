263319b5b088ea2c374ff2be059352e02e32f1b8
FwilsonRetro <fwilson@retrorabbit.co.za>
2021-02-11 15:55:25 +0200

    Redo of add players

--- lib/home/add_players.dart
+++ lib/home/add_players.dart
@@ -8,27 +8,6 @@ class AddPlayersFo extends StatefulWidget {
 class _AddPlayersFoState extends State<AddPlayersFo> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-          children: <Widget>[
-            DrawerHeader(
-              child: Text(
-                'Add New Player',
-                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-              ),
-              decoration: BoxDecoration(color: Colors.blue),
-            ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            )
-          ],
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
