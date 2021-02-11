27b95e6665d83bb0f0c5ddd4f93ed69174bc56dd
FwilsonRetro <fwilson@retrorabbit.co.za>
2021-02-11 16:36:43 +0200

    Continuation of adding players front end

--- lib/home/add_players.dart
+++ lib/home/add_players.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:screens_ui/shared/constants.dart';
 
 class AddPlayersFo extends StatefulWidget {
   @override
@@ -6,8 +7,36 @@ class AddPlayersFo extends StatefulWidget {
 }
 
 class _AddPlayersFoState extends State<AddPlayersFo> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Form(
+        key: _formKey,
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text("Add Player", style: TextStyle(fontSize: 18.0)),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextFormField(
+                decoration: textInputDecoration,
+                validator: (val) => val.isEmpty ? 'Please enter a name' : null,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextFormField(
+                decoration: textInputDecoration,
+                validator: (val) =>
+                    val.isEmpty ? 'Please enter a surname' : null,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
