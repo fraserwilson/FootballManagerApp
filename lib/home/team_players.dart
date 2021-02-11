623b49fc22c7cab983ad411898196b04423add31
FwilsonRetro <fwilson@retrorabbit.co.za>
2021-02-11 14:54:45 +0200

    Drawer added for adding players
    A drawer has been added where on button click a sub collection will be created in Firestore

--- lib/home/team_players.dart
+++ lib/home/team_players.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:screens_ui/home/add_players.dart';
 
 class TeamPlayers extends StatefulWidget {
   @override
@@ -9,12 +10,23 @@ class _TeamPlayersState extends State<TeamPlayers> {
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    void _showAddPlayer() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 60.0, horizontal: 60.0),
+              child: AddPlayersFo(),
+            );
+          });
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Squad Players'),
         actions: [
           FlatButton.icon(
-              onPressed: () {},
+              onPressed: () => _showAddPlayer(),
               icon: Icon(Icons.add),
               label: Text('Add Players'))
         ],
