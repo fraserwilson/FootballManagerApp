43e480281a9490035707bea6e983be6da5a5510b
FwilsonRetro <fwilson@retrorabbit.co.za>
2021-02-12 14:02:25 +0200

    Added padding to login screen
    Needed padding added

--- lib/authenticate/login.dart
+++ lib/authenticate/login.dart
@@ -37,101 +37,106 @@ class _LoginInScreenState extends State<LoginInScreen> {
                     label: Text("Register"))
               ],
             ),
-            body: Form(
-              key: _formKey,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 50.0,
-                    ),
-                    Center(
-                      child: CircleAvatar(
-                        backgroundImage: AssetImage('assets/soccerball.jpg'),
-                        backgroundColor: Colors.lightGreen[200],
-                        radius: 60.0,
+            body: Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+              child: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 50.0,
                       ),
-                    ),
-                    SizedBox(
-                      height: 50.0,
-                    ),
-                    TextFormField(
-                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                      obscureText: false,
-                      style: TextStyle(fontSize: 20.0),
-                      decoration: InputDecoration(
-                        contentPadding:
-                            EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                        hintText: 'Username',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(70.0)),
-                      ),
-                      onChanged: (val) {
-                        setState(() => email = val);
-                      },
-                    ),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-                    TextFormField(
-                      validator: (val) => val.length < 6
-                          ? 'Enter a password that is 6 characters long'
-                          : null,
-                      obscureText: true,
-                      style: TextStyle(fontSize: 20.0),
-                      decoration: InputDecoration(
-                        contentPadding:
-                            EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                        hintText: 'Password',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30.0)),
-                      ),
-                      onChanged: (val) {
-                        setState(() => password = val);
-                      },
-                    ),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-                    Material(
-                      elevation: 0.0,
-                      borderRadius: BorderRadius.circular(30.0),
-                      child: MaterialButton(
-                        padding: EdgeInsets.fromLTRB(50.0, 15.0, 50.0, 15.0),
-                        onPressed: () async {
-                          if (_formKey.currentState.validate()) {
-                            setState(() => loading = true);
-                            dynamic result = await _authService
-                                .signInWithEmailandPassword(email, password);
-                            if (result == null) {
-                              setState(() {
-                                error =
-                                    'Could not sign in with those credintials';
-                                loading = false;
-                              });
-                            }
-                          }
-                        },
-                        color: Colors.green[800],
-                        child: Text(
-                          "Login",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
+                      Center(
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage('assets/soccerball.jpg'),
+                          backgroundColor: Colors.lightGreen[200],
+                          radius: 60.0,
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 12.0,
-                    ),
-                    Text(
-                      error,
-                      style: TextStyle(
-                        color: Colors.red,
-                        fontSize: 14.0,
+                      SizedBox(
+                        height: 50.0,
                       ),
-                    ),
-                  ],
+                      TextFormField(
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter an email' : null,
+                        obscureText: false,
+                        style: TextStyle(fontSize: 20.0),
+                        decoration: InputDecoration(
+                          contentPadding:
+                              EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                          hintText: 'Username',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(70.0)),
+                        ),
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      TextFormField(
+                        validator: (val) => val.length < 6
+                            ? 'Enter a password that is 6 characters long'
+                            : null,
+                        obscureText: true,
+                        style: TextStyle(fontSize: 20.0),
+                        decoration: InputDecoration(
+                          contentPadding:
+                              EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                        ),
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      Material(
+                        elevation: 0.0,
+                        borderRadius: BorderRadius.circular(30.0),
+                        child: MaterialButton(
+                          padding: EdgeInsets.fromLTRB(50.0, 15.0, 50.0, 15.0),
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              setState(() => loading = true);
+                              dynamic result = await _authService
+                                  .signInWithEmailandPassword(email, password);
+                              if (result == null) {
+                                setState(() {
+                                  error =
+                                      'Could not sign in with those credintials';
+                                  loading = false;
+                                });
+                              }
+                            }
+                          },
+                          color: Colors.green[800],
+                          child: Text(
+                            "Login",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12.0,
+                      ),
+                      Text(
+                        error,
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
