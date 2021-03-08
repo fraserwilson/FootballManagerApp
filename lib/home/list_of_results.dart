6d0ab039a07873f69f655ccf48099974098f4aab
FwilsonRetro <fwilson@retrorabbit.co.za>
2021-03-08 07:11:57 +0200

    Calendar, struggling with null error

--- lib/home/list_of_results.dart
+++ lib/home/list_of_results.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:screens_ui/models/app_event.dart';
+import 'package:screens_ui/services/event_firestore_services.dart';
 import 'package:screens_ui/shared/loading.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -16,36 +18,21 @@ class _ListOfResultsState extends State<ListOfResults> {
         title: Text('List of Results'),
       ),
       body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('events').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
+          stream: eventDBS.streamList(),
+          builder: (context, snapshot) {
+            final events = snapshot.data;
             return ListView.builder(
-              itemCount: snapshot.data.docs.length,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: events.length,
               itemBuilder: (context, index) {
-                DocumentSnapshot docSnap = snapshot.data.docs[index];
-                return Padding(
-                    padding: EdgeInsets.only(top: 8.0),
-                    child: Card(
-                        margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
-                        child: ListTile(
-                            onTap: () {
-                              //ToDo
-                            },
-                            leading: CircleAvatar(
-                                backgroundColor: Colors.white,
-                                radius: 25.0,
-                                backgroundImage:
-                                    AssetImage('assets/player.png')),
-                            title: Text(docSnap['title']),
-                            subtitle: Text(
-                                'Position: ${docSnap['date'].toString()}'))));
+                AppEvent event = events[index];
+                return ListTile(
+                  title: Text(event.title),
+                );
               },
             );
-          } else {
-            return Loading();
-          }
-        },
-      ),
+          }),
     );
   }
 }
